b150a0766e2d1f919797fe1df5dcb1b79f049331
Ian Laird <irlaird@gmail.com>
2021-09-27 00:43:32 -0600

    Map ideas

---
+++ malibu/assets/world/sheets/forest.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="forest" tilewidth="16" tileheight="16" tilecount="390" columns="26">
+ <image source="forest.png" width="416" height="240"/>
+ <tile id="8" probability="0.5"/>
+ <tile id="9" probability="0.5"/>
+</tileset>
